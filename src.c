69751ea122a868835283e87f9fdf17111d23ef1a
Ayeen Poostforoushan <apoostforoushan@hotmail.com>
2023-01-01 11:49:01 +0330

    Final update of the bot
    The bot plays perfectly now, it uses the minimax algorithm to check 8 moves ahead and makes the best decision.

--- src.c
+++ src.c
@@ -7,98 +7,161 @@
 #define BLACK 0
 #define RED 1
 #define INF 99999
-#define MAX_DEPTH 4
-#define S_WIN 100
+#define S_WIN 1000
+#define DRAW -2
 
-void minimizerTurn(int board[6][7], int);
-void maximizerTurn(int board[6][7], int);
-int findBestMove(int board[6][7], int);
-int miniMax(int board[6][7], int, bool, int, int, int);
-int scoreOfBoard(int board[6][7], int);
-int winningSide(int board[6][7]);
-int checkForDirectionStreak(int board[6][7], int, int *, int, int);
+int runGame(int);
+int runBlackGame();
+int runRedGame();
+void minimizerTurn(int);
+void maximizerTurn(int);
+int findBestMove(int);
+int miniMax(int, bool, int, int, int);
+int scoreOfBoard(int);
+int winningSide();
+int checkForDirectionStreak(int, int *, int, int);
 void setAdditionForDirectionWin(int, int *, int *);
-int findHeight(int board[6][7], int);
-void clearBoard(int board[6][7]);
-void copyBoard(int board[6][7], int board2[6][7]);
-void printBoard(int board[6][7]);
+int findHeight(int);
+void clearBoard();
+void copyBoard(int board2[6][7]);
+void printBoard();
 int max(int, int);
 int min(int, int);
 
+int board[6][7];
+int MAX_DEPTH;
+
 int main()
 {
+
     int mySide;
     char s;
     scanf("%c", &s);
     if (s == 'b')
-        mySide = BLACK;
-    else
-        mySide = RED;
-
-    int board[6][7];
-    clearBoard(board);
-    while (1)
     {
-        minimizerTurn(board, !mySide);
-        printBoard(board);
-        int winner = winningSide(board);
-        if (winner == BLACK)
-        {
-            printf("BLACK WINS");
-            break;
-        }
-        else if (winner == RED)
-        {
-            printf("RED WINS");
-            break;
-        }
-        maximizerTurn(board, mySide);
-        printf("BOT:\n");
-        printBoard(board);
-        winner = winningSide(board);
-        if (winner == BLACK)
-        {
-            printf("BLACK WINS");
-            break;
-        }
-        else if (winner == RED)
-        {
-            printf("RED WINS");
-            break;
-        }
+        mySide = BLACK;
+        MAX_DEPTH = 7;
     }
+    else
+    {
+        mySide = RED;
+        MAX_DEPTH = 8;
+    }
+    clearBoard();
+    int a = runGame(mySide);
+    printf("WIN: %d", a);
 
     return 0;
 }
 
-void minimizerTurn(int board[6][7], int side)
+int runGame(int mySide)
+{
+    if (mySide == BLACK)
+        return runBlackGame();
+    else
+        return runRedGame();
+}
+
+int runBlackGame()
+{
+    while (1)
+    {
+        maximizerTurn(BLACK);
+        //printBoard();
+        int winner = winningSide();
+        if (winner == BLACK)
+        {
+            return BLACK;
+        }
+        else if (winner == RED)
+        {
+            return RED;
+        }
+        else if (winner == DRAW)
+        {
+            return DRAW;
+        }
+        minimizerTurn(RED);
+        winner = winningSide();
+        if (winner == BLACK)
+        {
+            return BLACK;
+        }
+        else if (winner == RED)
+        {
+            return RED;
+        }
+        else if (winner == DRAW)
+        {
+            return DRAW;
+        }
+    }
+}
+
+int runRedGame()
+{
+    while (1)
+    {
+        minimizerTurn(BLACK);
+        int winner = winningSide();
+        if (winner == BLACK)
+        {
+            return BLACK;
+        }
+        else if (winner == RED)
+        {
+            return RED;
+        }
+        else if (winner == DRAW)
+        {
+            return DRAW;
+        }
+        maximizerTurn(RED);
+        //printBoard();
+        winner = winningSide();
+        if (winner == BLACK)
+        {
+            return BLACK;
+        }
+        else if (winner == RED)
+        {
+            return RED;
+        }
+        else if (winner == DRAW)
+        {
+            return DRAW;
+        }
+    }
+}
+
+void minimizerTurn(int side)
 {
     int moveInput;
     scanf("%d", &moveInput);
-    board[findHeight(board, moveInput - 1)][moveInput - 1] = side;
+    board[findHeight(moveInput - 1)][moveInput - 1] = side;
 }
 
-void maximizerTurn(int board[6][7], int side)
+void maximizerTurn(int side)
 {
-    int moveIndex = findBestMove(board, side);
-    board[findHeight(board, moveIndex)][moveIndex] = side;
-    // int moveInput;
-    // scanf("%d", &moveInput);
-    // board[findHeight(board, moveInput - 1)][moveInput - 1] = side;
+    int moveIndex = findBestMove(side);
+    printf("%d", moveIndex + 1);
+    board[findHeight(moveIndex)][moveIndex] = side;
 }
 
-int findBestMove(int board[6][7], int side)
+int findBestMove(int side)
 {
     int bestMoveIndex = 0;
     int bestMoveScore = -1 * INF;
     int alpha = bestMoveScore;
     for (int j = 0; j < 7; j++)
     {
-        int changedCellHeight = findHeight(board, j);
+        int changedCellHeight = findHeight(j);
+        if (changedCellHeight > 5)
+            continue;
         board[changedCellHeight][j] = side;
-        int score = miniMax(board, side, false, 0, alpha, INF);
+        int score = miniMax(side, false, 0, alpha, INF);
         board[changedCellHeight][j] = EMPTY;
-        printf("BMS: %4d\tBMI: %d\tMAX %d: %d\n", bestMoveScore, bestMoveIndex, j, score);
+        // printf("BMS: %4d\tBMI: %d\tMAX %d: %d\n", bestMoveScore, bestMoveIndex, j, score);
         if (bestMoveScore < score)
         {
             bestMoveScore = score;
@@ -109,30 +172,30 @@ int findBestMove(int board[6][7], int side)
     return bestMoveIndex;
 }
 
-int miniMax(int board[6][7], int side, bool isMaximizer, int depth, int alpha, int beta)
+int miniMax(int side, bool isMaximizer, int depth, int alpha, int beta)
 {
+    // Terminating conditions
     if (depth == MAX_DEPTH)
-    {
-        // printf("\n");
-        // printBoard(board);
-        return scoreOfBoard(board, side);
-    }
-    int winner = winningSide(board);
+        return scoreOfBoard(side);
+    int winner = winningSide();
     if (winner == side)
-        return S_WIN;
+        return S_WIN - depth;
     else if (winner == !side)
-        return -1 * S_WIN;
+        return -1 * S_WIN + depth;
+    else if (winner == DRAW)
+        return 0;
     int bestValue;
     if (isMaximizer)
     {
         bestValue = -1 * INF;
         for (int j = 0; j < 7; j++)
         {
-            int changedCellHeight = findHeight(board, j);
+            int changedCellHeight = findHeight(j);
+            if (changedCellHeight > 5)
+                continue;
             board[changedCellHeight][j] = side;
-            int score = miniMax(board, side, false, depth + 1, alpha, beta);
+            int score = miniMax(side, false, depth + 1, alpha, beta);
             board[changedCellHeight][j] = EMPTY;
-            // printf("{%d: %d} ", j, score);
             bestValue = max(bestValue, score);
             alpha = max(bestValue, alpha);
             if (alpha >= beta)
@@ -144,29 +207,29 @@ int miniMax(int board[6][7], int side, bool isMaximizer, int depth, int alpha, i
         bestValue = INF;
         for (int j = 0; j < 7; j++)
         {
-            int changedCellHeight = findHeight(board, j);
+            int changedCellHeight = findHeight(j);
+            if (changedCellHeight > 5)
+                continue;
             board[changedCellHeight][j] = !side;
-            int score = miniMax(board, side, true, depth + 1, alpha, beta);
+            int score = miniMax(side, true, depth + 1, alpha, beta);
             board[changedCellHeight][j] = EMPTY;
-            // printf("[%d: %d] ", j, score);
             bestValue = min(bestValue, score);
             beta = min(bestValue, beta);
             if (alpha >= beta)
                 break;
         }
     }
-    // printf("\n");
     return bestValue;
 }
 
-int scoreOfBoard(int board[6][7], int side)
+int scoreOfBoard(int side)
 {
     int score = 0;
-    int result = winningSide(board);
+    int result = winningSide();
     if (result == side)
-        return S_WIN;
+        return S_WIN - MAX_DEPTH;
     else if (result == !side)
-        return -1 * S_WIN;
+        return -1 * S_WIN + MAX_DEPTH;
     // Check for double cells
     for (int i = 0; i < 6; i++)
     {
@@ -184,7 +247,7 @@ int scoreOfBoard(int board[6][7], int side)
                 {
                     int additionToScore = 0;
                     int blockedState = 0;
-                    int streak = checkForDirectionStreak(board, dir, &blockedState, i, j);
+                    int streak = checkForDirectionStreak(dir, &blockedState, i, j);
                     if (streak >= 2)
                     {
                         if (blockedState == 0)
@@ -202,7 +265,7 @@ int scoreOfBoard(int board[6][7], int side)
     return score;
 }
 
-int winningSide(int board[6][7])
+int winningSide()
 {
     int x;
     for (int i = 0; i < 6; i++)
@@ -213,7 +276,7 @@ int winningSide(int board[6][7])
             {
                 for (int dir = 1; dir <= 4; dir++)
                 {
-                    int result = checkForDirectionStreak(board, dir, &x, i, j);
+                    int result = checkForDirectionStreak(dir, &x, i, j);
                     if (result == 4)
                     {
                         return board[i][j];
@@ -222,10 +285,21 @@ int winningSide(int board[6][7])
             }
         }
     }
+    int isDraw = 1;
+    for (int i = 0; i < 7; i++)
+    {
+        if (board[5][i] == EMPTY)
+        {
+            isDraw = 0;
+            break;
+        }
+    }
+    if (isDraw)
+        return DRAW;
     return EMPTY;
 }
 
-int checkForDirectionStreak(int board[6][7], int direction, int *blockedState, int i, int j)
+int checkForDirectionStreak(int direction, int *blockedState, int i, int j)
 {
     int side = board[i][j];
     int iAddition, jAddition;
@@ -235,7 +309,7 @@ int checkForDirectionStreak(int board[6][7], int direction, int *blockedState, i
     int iLast2 = i - 2 * iAddition;
     int jLast2 = j - 2 * jAddition;
     bool last2IsBlocked = 0;
-    if (iLast < 0 || jLast > 6 || jLast < 0 || board[iLast][jLast] == !side)
+    if (iLast < 0 || jLast > 6 || jLast < 0 || board[iLast][jLast] != EMPTY)
         *blockedState = *blockedState + 1;
     else if (iLast2 < 0 || jLast2 > 6 || jLast2 < 0 || board[iLast2][jLast2] == !side)
         last2IsBlocked = 1;
@@ -293,7 +367,7 @@ void setAdditionForDirectionWin(int direction, int *iAddition, int *jAddition)
     }
 }
 
-int findHeight(int board[6][7], int moveIndex)
+int findHeight(int moveIndex)
 {
     int height = 5;
     while (height >= 0)
@@ -305,7 +379,7 @@ int findHeight(int board[6][7], int moveIndex)
     return height + 1;
 }
 
-void clearBoard(int board[6][7])
+void clearBoard()
 {
     for (int i = 0; i < 6; i++)
     {
@@ -316,7 +390,7 @@ void clearBoard(int board[6][7])
     }
 }
 
-void copyBoard(int board[6][7], int board2[6][7])
+void copyBoard(int board2[6][7])
 {
     for (int i = 0; i < 6; i++)
     {
@@ -327,7 +401,7 @@ void copyBoard(int board[6][7], int board2[6][7])
     }
 }
 
-void printBoard(int board[6][7])
+void printBoard()
 {
     for (int i = 5; i >= 0; i--)
     {
